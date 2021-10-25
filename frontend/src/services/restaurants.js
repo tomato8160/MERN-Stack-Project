80b162244d0cd167f554f70a3bc094d813f1d852
세현 이 <tpgus2303@naver.com>
2021-10-25 22:46:49 +0900

    start frontend

---
+++ frontend/src/services/restaurants.js
@@ -0,0 +1,28 @@
+import http from "../http-common";
+
+class RestaurantsDataService {
+    getAll(page =0){
+        return http.get(`?page=${page}`)
+    }
+    get(id){
+        return http.get(`/id/=${id}`)
+    }
+    find(query, by = "name", page = 0){
+        return http.get(`?${by}=${query}&page=${page}`)
+    }
+    createReview(data){
+        return http.post("/review", data)
+    }
+    updateReview(data){
+        return http.put("review",data)
+    }
+    deleteReview(id){
+        return http.delete(`/review?id=${id}`)
+    }
+    getCuisines(id){
+        return http.get(`/cuisines`)
+    }
+    
+}
+
+export default new RestaurantsDataService();
